80a7f35d8ba9d55d8fa7f57dba23ae93968d42a8
6yntar05 <6yntar05@mail.ru>
2023-10-18 20:30:22 +0700

    rm.sh: removal to trash

--- usr/local/bin/rm.sh
+++ usr/local/bin/rm.sh
@@ -1,6 +1,59 @@
-#!/bin/sh
-#Carelessness protection
+#!/bin/bash
+# Carelessness protection
+
+is_permanent=true
+args=""
+OPTIND=1
+
+while getopts ":rt-:" opt; do
+    case $opt in
+        -)
+            case "${OPTARG}" in
+                perm)
+                    is_permanent=true
+                    ;;
+                trash)
+                    is_permanent=false
+                    ;;
+                help)
+                    printf -- "rm.sh : carelessness protection script\n"
+                    printf -- "move files to ~/.trash and print target files with additional confirm\n"
+                    printf -- "Usage: rm (args) (other args) [files]\n"
+                    printf -- "\t--help     : show help message\n"
+                    printf -- "\t--perm     : remove files permanently\n"
+                    printf -- "\t--trash -t : move files to trash\n"
+                    printf -- "\t  ...      : other rm/mv args"
+                    exit 1
+                    ;;
+                *)
+                    args+=" --${OPTARG}"
+                    ;;
+            esac
+            ;;
+        t)
+            is_permanent=false
+            ;;
+        r)
+            if [ "$is_permanent" = true ]; then
+                args+=" -r"
+            fi
+            ;;
+        \?)
+            args+=" -$OPTARG"
+            ;;
+    esac
+done
+shift "$((OPTIND-1))"
 
 echo "[!!] Removing: ${*}" >&2
+echo "Args:$args"
 read -rp '> Hit ^C to cancel'
-/bin/rm "${@}"
+
+if [ "$is_permanent" = true ]; then
+    /bin/rm $args -- "${@}"
+else
+    suffix=$(date +%Y-%m-%d-%H-%M-%S)
+    mkdir -p ~/.trash/"${suffix}"
+    /bin/mv $args -- "${@}" ~/.trash/"${suffix}"/
+fi
+exit $?
